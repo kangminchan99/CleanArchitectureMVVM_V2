d0f25ff01cf66b0d74d134ac93e69dabc375cd0a
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-09-01 18:40:24 +0900

    Sign Up Screen

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -4,6 +4,7 @@ import 'package:cleanarchitecture_v2/domain/model/recipe_model.dart';
 import 'package:cleanarchitecture_v2/domain/usecase/get_saved_recipes_usecase.dart';
 import 'package:cleanarchitecture_v2/presentation/saved_recipes/saved_recipes_screen.dart';
 import 'package:cleanarchitecture_v2/presentation/sign_in/sign_in_screen.dart';
+import 'package:cleanarchitecture_v2/presentation/sign_up/sign_up_screen.dart';
 import 'package:cleanarchitecture_v2/presentation/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -20,6 +21,10 @@ final router = GoRouter(
     GoRoute(
       path: SignInScreen.routeURL,
       builder: (context, state) => SignInScreen(),
+    ),
+    GoRoute(
+      path: SignUpScreen.routeURL,
+      builder: (context, state) => SignUpScreen(),
     ),
     GoRoute(
       path: SavedRecipesScreen.routeURL,
---
+++ lib/presentation/sign_up/sign_up_screen.dart
@@ -0,0 +1,147 @@
+import 'package:cleanarchitecture_v2/core/presentation/components/big_button.dart';
+import 'package:cleanarchitecture_v2/core/presentation/components/input_field.dart';
+import 'package:cleanarchitecture_v2/ui/color_styles.dart';
+import 'package:cleanarchitecture_v2/ui/gaps.dart';
+import 'package:cleanarchitecture_v2/ui/sizes.dart';
+import 'package:cleanarchitecture_v2/ui/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  static const routeURL = '/sign_up';
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  bool _isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(Sizes.size20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Create an account',
+                  style: TextStyles.largeTextBold,
+                ),
+                SizedBox(
+                  width: 200,
+                  child: Text(
+                    'Lets help you set up your account, it wont take long.',
+                    style: TextStyles.smallerTextRegular,
+                  ),
+                ),
+                Gaps.v20,
+                InputField(label: 'Name', placeHolder: 'Enter Name'),
+                Gaps.v20,
+                InputField(label: 'Email', placeHolder: 'Enter Email'),
+                Gaps.v20,
+                InputField(
+                  label: 'Password',
+                  placeHolder: 'Enter password',
+                ),
+                Gaps.v20,
+                InputField(
+                  label: 'Confirm Password',
+                  placeHolder: 'Retype password',
+                ),
+                Gaps.v20,
+                Row(
+                  children: [
+                    Checkbox(
+                      value: _isChecked,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(Sizes.size5),
+                      ),
+                      side: BorderSide(
+                        width: Sizes.size1,
+                        color: ColorStyles.secondary100,
+                      ),
+                      activeColor: ColorStyles.secondary100,
+                      onChanged: (value) {
+                        setState(() {
+                          _isChecked = value!;
+                        });
+                      },
+                    ),
+                    Text(
+                      'Accept terms & Condition',
+                      style: TextStyles.smallerTextRegular.copyWith(
+                        color: ColorStyles.secondary100,
+                      ),
+                    ),
+                  ],
+                ),
+                Gaps.v24,
+                BigButton(text: 'Sign Up', onPressed: () {}),
+                Gaps.v20,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: Sizes.size52,
+                      height: Sizes.size1,
+                      color: ColorStyles.gray4,
+                    ),
+                    Gaps.h8,
+                    Text(
+                      'Or Sign in With',
+                      style: TextStyles.smallerTextBold.copyWith(
+                        color: ColorStyles.gray4,
+                      ),
+                    ),
+                    Gaps.h8,
+                    Container(
+                      width: Sizes.size52,
+                      height: Sizes.size1,
+                      color: ColorStyles.gray4,
+                    ),
+                  ],
+                ),
+                Gaps.v20,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/login/google_button.png',
+                      width: Sizes.size60,
+                      height: Sizes.size60,
+                    ),
+                    Gaps.h16,
+                    Image.asset(
+                      'assets/images/login/facebook_button.png',
+                      width: Sizes.size60,
+                      height: Sizes.size60,
+                    ),
+                  ],
+                ),
+                Gaps.v20,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already a member?',
+                      style: TextStyles.smallerTextBold,
+                    ),
+                    Text(
+                      'Sign In',
+                      style: TextStyles.smallerTextBold.copyWith(
+                        color: ColorStyles.secondary100,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
